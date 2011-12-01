8244e89e4d3cdc3c88f305dec4506219e804bc8d
Sven Michael Klose <pixel@copei.de>
2011-12-01 14:14:26 +0100

    Cleanup.

--- admin/cms-config.php
+++ admin/cms-config.php
@@ -7,7 +7,6 @@
 #
 # Licensed under the MIT, BSD and GPL licenses.
 
-
 $cms_root_table = 'directories';
 $cms_root_id = 1;
 
@@ -39,7 +38,6 @@ $cms_object_editors = array ('d_order_duty' => 'edit_duty_fields',
 $cms_object_views = array ('u_attrib_mask' => 'product_attrib_mask_view');
 
 # Virtual directory aliases for link creation.
-# TODO: Move this into dbconf.class managed area.
 $vdir_name = array ('Warenkorb' => 'CART', 'Bestellung' => 'ORDER');
 $vdir_alias = array ('CART' => 'Warenkorb', 'ORDER' => 'Bestellung');
 $vdir_name['Suche'] = 'SEARCH';
